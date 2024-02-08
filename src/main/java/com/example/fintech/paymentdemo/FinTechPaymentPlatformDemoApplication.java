b7b8e061ecac3301fec5194f1f26d08deb5f0cff
Haitian Hao (Harry) <harryhao97@gmail.com>
2024-02-08 18:40:20 -0500

    Update FinTechPaymentPlatformDemoApplication.java

--- src/main/java/com/example/fintech/paymentdemo/FinTechPaymentPlatformDemoApplication.java
+++ src/main/java/com/example/fintech/paymentdemo/FinTechPaymentPlatformDemoApplication.java
@@ -10,21 +10,7 @@ import java.util.Arrays;
 public class FinTechPaymentPlatformDemoApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(FinTechPaymentPlatformDemoApplication.class, args);
-		FinTechPaymentPlatformDemoApplication a = new FinTechPaymentPlatformDemoApplication();
-		int[] output=a.twoSum(new int[]{1,2,3,4,5}, 5);
-		System.out.println(Arrays.toString(output));
-	}
-
-	public int[] twoSum(int[] nums, int target) {
-		ArrayList<Integer> arr = new ArrayList<>();
-		for (int i=0; i< nums.length; i++){
-			for (int j = 1; j<nums.length; j++){
-				if (nums[i]+nums[j]==target)
-					return new int[]{nums[i], nums[j]};
-			}
-		}
-		return new int[]{-1};
+		SpringApplication.run(FinTechPaymentPlatformDemoApplication.class, args);
 	}
 
 
